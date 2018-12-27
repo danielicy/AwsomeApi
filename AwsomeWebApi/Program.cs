9ca7a9186ced706f4282dd1ef87e211892fc1c62
danielic@razorgrip.com <c@e>
2018-12-27 15:33:29 +0200

    awsome project

---
+++ AwsomeWebApi/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace AwsomeWebApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            BuildCustomWebHost(args).Run();
+           // BuildWebHost(args).Run();
+            // CreateWebHostBuilder(args).Build().Run();
+        }
+
+
+        //=========================
+        public static IWebHost BuildCustomWebHost(string[] args) =>
+            new WebHostBuilder().UseKestrel()
+            .UseContentRoot(Directory
+                .GetCurrentDirectory())
+            .ConfigureAppConfiguration(config => config.AddJsonFile("appSettings.json", true)
+            )
+            .ConfigureLogging(logging =>
+            logging.AddConsole().AddDebug()
+            )
+            .UseIISIntegration()
+            .UseStartup<Startup>()
+            .Build();
+
+        //=========================================
+
+     /*   public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>()
+            .Build();*/
+
+        /*public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();*/
+    }
+}
