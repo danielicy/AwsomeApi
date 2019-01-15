5106a6483959cde195d7aef311112c72191cea25
danielic@razorgrip.com <c@e>
2019-01-15 10:35:37 +0200

    MapRoute

--- AwsomeWebApi/Startup.cs
+++ AwsomeWebApi/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Routing;
 
 namespace AwsomeWebApi
 {
@@ -18,6 +19,7 @@ namespace AwsomeWebApi
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRouting();
 
             services.AddSingleton<IComponent, ComponentB>();
         }
@@ -32,64 +34,101 @@ namespace AwsomeWebApi
                  app.UseDeveloperExceptionPage();
              }
 
-           /* app.Map("/skip", (skipApp) => skipApp.Run(async (context) => 
-            await context.Response.WriteAsync($"Skip the line!")));*/
-
-
-            app.Map("/skip", (skipApp) => skipApp.UseSkipApp());
-            app.UseNumberChecker();
-            app.UseUpperValue();
-            app.UseVowelMasker();
-            app.Run(async (context) =>
+            app.UseRouter(builder =>
             {
-                var value = context.Items["value"].ToString();
-                await context.Response.WriteAsync($"You entered a string:{ value}");
+                builder.MapRoute(string.Empty, context =>
+                {
+                    return context.Response.WriteAsync($"Welcome to the default route!");
+                });
+
+                //http://localhost:55428/foo/john/lennon
+                builder.MapGet("foo/{name}/{surname?}", (request, response, routeData) =>
+                {
+                    return response.WriteAsync($"Welcome to Foo, {routeData.Values["name"]} {routeData.Values["surname"]}");
+                });
+
+                builder.MapPost("bar/{number:int}", (request, response, routeData) =>
+                {
+                    return response.WriteAsync($"Welcome to Bar, number is {routeData.Values["number"]}");
+                });
             });
-        
-        /*  app.Use(async (context, next) =>
-          {
-              var value = context.Request.Query["value"].ToString();
-              if (int.TryParse(value, out int intValue))
+
+            /* app.Map("/skip", (skipApp) => skipApp.Run(async (context) => 
+             await context.Response.WriteAsync($"Skip the line!")));*/
+
+
+            /* app.Map("/skip", (skipApp) => skipApp.UseSkipApp());
+             app.UseNumberChecker();
+             app.UseUpperValue();
+             app.UseVowelMasker();
+
+             app.Map("/foo",
+                config =>
+                config.Use(async (context, next) =>
+                await context.Response.WriteAsync("<div><h1>Mexico</h1>" +
+                "<p>Macho Man</p></div>")
+                ));
+
+             app.MapWhen(
+                 context =>
+                 context.Request.Method == "POST" &&
+                 context.Request.Path == "/bar",
+                 config =>
+                 config.Use(async (context, next) =>
+                 await context.Response.WriteAsync("Welcome to POST /bar")
+                 )
+                 );
+
+             app.Run(async (context) =>
+             {
+                 var value = context.Items["value"].ToString();
+                 await context.Response.WriteAsync($"You entered a string:{ value}");
+             });*/
+
+            /*  app.Use(async (context, next) =>
               {
-                  await context.Response.WriteAsync($"You entered a number:{intValue}");
-              }
-              else
+                  var value = context.Request.Query["value"].ToString();
+                  if (int.TryParse(value, out int intValue))
+                  {
+                      await context.Response.WriteAsync($"You entered a number:{intValue}");
+                  }
+                  else
+                  {
+                      context.Items["value"] = value;
+                      await next();
+                  }
+              });
+
+              app.Use(async (context, next) =>
               {
-                  context.Items["value"] = value;
+                  var value = context.Items["value"].ToString();
+                  if (int.TryParse(value, out int intValue))
+                  {
+                      await context.Response.WriteAsync($"You entered a number:{ intValue}");
+    }
+                  else
+                  {
+                      await next();
+                  }
+              });
+
+              app.Use(async (context, next) =>
+              {
+                  var value = context.Items["value"].ToString();
+                  context.Items["value"] = value.ToUpper();
                   await next();
-              }
-          });
-
-          app.Use(async (context, next) =>
-          {
-              var value = context.Items["value"].ToString();
-              if (int.TryParse(value, out int intValue))
+              });
+              app.Use(async (context, next) =>
               {
-                  await context.Response.WriteAsync($"You entered a number:{ intValue}");
-}
-              else
-              {
+                  var value = context.Items["value"].ToString();
+                  context.Items["value"] = Regex.Replace(value, "(?<!^)[AEUI](?!$)",
+                  "*");
                   await next();
-              }
-          });
+              });*/
 
-          app.Use(async (context, next) =>
-          {
-              var value = context.Items["value"].ToString();
-              context.Items["value"] = value.ToUpper();
-              await next();
-          });
-          app.Use(async (context, next) =>
-          {
-              var value = context.Items["value"].ToString();
-              context.Items["value"] = Regex.Replace(value, "(?<!^)[AEUI](?!$)",
-              "*");
-              await next();
-          });*/
+            //==================================================
 
-        //==================================================
-
-        app.Map("/foo",
+          /*  app.Map("/foo",
                 config =>
                 config.Use(async (context, next) =>
                 await context.Response.WriteAsync("<div><h1>Mexico</h1>" +
@@ -104,7 +143,7 @@ namespace AwsomeWebApi
                 config.Use(async (context, next) =>
                 await context.Response.WriteAsync("Welcome to POST /bar")
                 )
-                );
+                );*/
             //--------------------------------------------------------------
             /*app.Run(async (context) =>
             await context.Response.WriteAsync($"Welcome to the default")
